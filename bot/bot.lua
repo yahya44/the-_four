65b7101a8fbe1510c21d84d1e18178b736dcabe5
yahya44 <yahyakaramiazad@Gmail.com>
2016-10-28 09:51:45 +0100

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {119711876},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
@@ -291,3 +291,4 @@ our_id = 0
 now = os.time()
 math.randomseed(now)
 started = false
+I
